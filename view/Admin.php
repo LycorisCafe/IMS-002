679b138f2727aa6cede8f1e57578d05f51a4e0a8
DasunNethsara-04 <techsaralk.pro@gmail.com>
2022-12-29 20:15:12 +0530

    Structure changes

--- view/Admin.php
+++ view/Admin.php
@@ -14,6 +14,52 @@ if (isset($_SESSION['id']) && isset($_SESSION['role'])) {
 
     <body>
         <?php require_once "../req/navbar.php"; ?>
+        
+        <div class="row" >
+    	<div class="col-md-10" ><!-- left column -->
+        	<center><h4>Monthly Student Registration</h4></center>
+            <canvas id="lineChart" width="800" height="450"></canvas>
+         </div>  
+     </div>
+		
+<script>
+                    
+function showLineChart(monthly_std_reg){	
+ 
+	var monthly_std_reg1 = JSON.parse("[" + monthly_std_reg + "]");
+ 
+	new Chart(document.getElementById("lineChart"), {
+		type: 'line',
+		data: {
+		  labels: ["January", "February", "March", "April", "May", "June","July", "August", "September", "October", "November", "December"],
+		  datasets: [
+			{
+			  label: "New Student Registration",
+			  borderColor: "#00c0ef",
+			  fill: false,
+			  data: monthly_std_reg1
+			 
+			}
+		  ]
+		},
+		options: {
+		  legend: { display: false },
+		  title: {
+			display: true,
+			text: ''
+		  },
+		  scales: {
+				yAxes: [{
+					ticks: {
+						beginAtZero:true
+					}
+				}]
+			}
+		}
+	});
+
+};
+</script>
     </body>
 
     </html>
