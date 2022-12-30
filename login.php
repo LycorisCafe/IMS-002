23c0ed6906c6eb77ce7c8dafaf0096e469ae7afb
DasunNethsara-04 <techsaralk.pro@gmail.com>
2022-12-30 03:46:08 -0800

    Moderator backend: Username search and grabbing all the data (std name, std pic), std image upload finished. New UI updates in all pages. Session Management fixed. Yahooooooooooooooooo

--- login.php
+++ login.php
@@ -4,7 +4,11 @@
 				<meta charset="utf-8">
 				<meta name="viewport" content="width=device-width, initial-scale=1">
 				<title>Login</title>
-				<link href="bootstrap/css/bootstrap.min.css" rel="stylesheet">
+				<link href="../bootstrap/css/bootstrap.min.css" rel="stylesheet">
+				<link rel="stylesheet" href="css/fonts.css">
+				<link href="https://www.sftthaksalawa.com/home/vendor/bootstrap/css/bootstrap.min.css" rel="stylesheet">
+				<link rel="stylesheet" href="css/temp.css">
+				<script src="fontawesome.com.js" crossorigin="anonymous"></script>
 				<style>
 						body {
 								background-image: url('Media/images/bg.jpg');
@@ -15,31 +19,21 @@
 				</style>
 		</head>
 		<body>
-				<nav class="navbar navbar-expand-md navbar-dark bg-dark mb-4">
-						<div class="container-fluid">
-								<a class="navbar-brand" href="https://sftthaksalawa.com" style="font-weight: 900;"><span style="color: rgb(0, 191, 161);">SFT</span> තක්සලාව</a>
-							<button class="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarCollapse" aria-controls="navbarCollapse" aria-expanded="false" aria-label="Toggle navigation">
-								<span class="navbar-toggler-icon"></span>
-							</button>
-							<div class="collapse navbar-collapse" id="navbarCollapse">
-								<ul class="navbar-nav me-auto mb-2 mb-md-0">
-									<li class="nav-item">
-										<a class="nav-link active" aria-current="page" href="index.html">Home</a>
-									</li>
-									<li class="nav-item">
-										<a class="nav-link" href="login.php">Login</a>
-									</li>
-									<li class="nav-item">
-											<a class="nav-link" href="aboutUs.html">About Us</a>
-									</li>
-								</ul>
-								<form class="d-flex">
-									<input class="form-control me-2" type="search" placeholder="Search" aria-label="Search">
-									<button class="btn btn-outline-success" type="submit">Search</button>
-								</form>
-							</div>
-						</div>
-					</nav>
+			<header class="main-header clearfix" role="header">
+                <div class="logo">
+                    <a href="index.html"><em>SFT</em> තක්සලාව</a>
+                </div>
+                <a href="" class="menu-link"><i class="fa fa-bars"></i></a>
+                <nav id="menu" class="main-nav" role="navigation">
+                    <ul class="main-menu">
+                        <li><a href="index.html">Home</a></li>
+        
+                        <li><a href="aboutUs.html">About Us</a></li>
+                        <li><a href="login.php">Login</a></li>
+        
+                    </ul>
+                </nav>
+            </header>
 				
 				<div class="container col-lg-4 col-md-5 align-self-center">
 						<div class="card" style="transform: translate(0%, 50%);">
--- view/Moderator.php
+++ view/Moderator.php
@@ -12,28 +12,26 @@ if (isset($_SESSION['id']) && isset($_SESSION['role'])) {
         <meta name="viewport" content="width=device-width, initial-scale=1">
         <title>Moderator</title>
         <link href="../bootstrap/css/bootstrap.min.css" rel="stylesheet">
+        <link rel="stylesheet" href="../css/fonts.css">
+        <link href="https://www.sftthaksalawa.com/home/vendor/bootstrap/css/bootstrap.min.css" rel="stylesheet">
+        <link rel="stylesheet" href="../css/temp.css">
+        <script src="../fontawesome.com.js" crossorigin="anonymous"></script>
     </head>
 
     <body>
-    <nav class="navbar navbar-expand-md navbar-dark bg-dark mb-4">
-            <div class="container-fluid">
-                <a class="navbar-brand" href="https://sftthaksalawa.com" style="font-weight: 900;"><span style="color: rgb(0, 191, 161);">SFT</span> තක්සලාව</a>
-              <button class="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarCollapse" aria-controls="navbarCollapse" aria-expanded="false" aria-label="Toggle navigation">
-                <span class="navbar-toggler-icon"></span>
-              </button>
-              <div class="collapse navbar-collapse" id="navbarCollapse">
-                <ul class="navbar-nav me-auto mb-2 mb-md-0">
-                <li class="nav-item">
-                        <b style='color: white;'>User: <?php echo $_SESSION['fname']; ?></b>
-                  </li>
-                  <li class="nav-item">
-                        <b style='color: white;'>Role: <?php echo $_SESSION['role']; ?></b>
-                  </li>
-                </ul>
-                <a href='../req/logout.php' class='btn btn-danger'> Logout </a>
-              </div>
-            </div>
-          </nav>
+    <header class="main-header clearfix" role="header">
+        <div class="logo">
+            <a href="index.html"><em>SFT</em> තක්සලාව</a>
+        </div>
+        <a href="" class="menu-link"><i class="fa fa-bars"></i></a>
+        <nav id="menu" class="main-nav" role="navigation">
+            <ul class="main-menu">
+            <li style='color: #fff;'>User: <?php echo $_SESSION['fname']; ?></li>
+                <li style='color: #fff;'>Role: <?php echo $_SESSION['role']; ?></li>
+
+            </ul>
+        </nav>
+    </header>
         <br><br>
         <div class="text-center">
             <h1 class="display-6">SFT තක්සලාව</h1>
