e0a661112573a0afca83ccadc067395a5a9e0bf4
naveenb2004 <naveennbalasooriya2004@gmail.com>
2023-01-01 13:42:10 +0530

    admin nav updates

--- view/Admin.php
+++ view/Admin.php
@@ -2,55 +2,48 @@
 session_start();
 if (isset($_SESSION['id']) && isset($_SESSION['role'])) {
 ?>
-<!doctype html>
-<html lang="en">
+    <!doctype html>
+    <html lang="en" data-bs-theme="dark">
 
-<head>
-    <meta charset="utf-8">
-    <meta name="viewport" content="width=device-width, initial-scale=1">
-    <title>Administrator Panel</title>
-    <link href="../bootstrap/css/bootstrap.min.css" rel="stylesheet">
-</head>
+    <head>
+        <meta charset="utf-8">
+        <meta name="viewport" content="width=device-width, initial-scale=1">
+        <title>Administrator Panel</title>
+        <link href="../bootstrap/css/bootstrap.min.css" rel="stylesheet">
+    </head>
 
-<body>
+    <body>
 
-    <nav class="navbar navbar-expand-lg bg-body-tertiary">
-        <div class="container-fluid">
-            <button class="btn btn-primary text-start" type="button" data-bs-toggle="offcanvas" data-bs-target="#offcanvasRight" aria-controls="offcanvasRight">-/-</button>
-            <a class="navbar-brand" href="#">ClassName</a>
-            <button class="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarNavAltMarkup" aria-controls="navbarNavAltMarkup" aria-expanded="false" aria-label="Toggle navigation">
-                <span class="navbar-toggler-icon"></span>
-            </button>
-            <div class="collapse navbar-collapse" id="navbarNavAltMarkup">
-                <div class="navbar-nav">
-                    <a class="btn btn-danger" aria-current="page" href="../req/logout.php">Logout</a>
-                </div>
+        <nav class="navbar bg-body-tertiary">
+            <div class="container-fluid">
+                <button class="btn btn-outline-primary" type="button" data-bs-toggle="offcanvas" data-bs-target="#offcanvasWithBothOptions" aria-controls="offcanvasExample">
+                    <svg xmlns="http://www.w3.org/2000/svg" width="16" height="16" fill="currentColor" class="bi bi-cloud-haze2-fill" viewBox="0 0 16 16">
+                        <path d="M8.5 2a5.001 5.001 0 0 1 4.905 4.027A3 3 0 0 1 13 12H3.5A3.5 3.5 0 0 1 .035 9H5.5a.5.5 0 0 0 0-1H.035a3.5 3.5 0 0 1 3.871-2.977A5.001 5.001 0 0 1 8.5 2zm-6 8a.5.5 0 0 0 0 1h9a.5.5 0 0 0 0-1h-9zM0 13.5a.5.5 0 0 1 .5-.5h9a.5.5 0 0 1 0 1h-9a.5.5 0 0 1-.5-.5z" />
+                    </svg>
+                </button>
+                <a class="navbar-brand" href="#">
+                    <img src="../Media/dummy.jpg" alt="Logo" width="30" height="30" class="d-inline-block align-text-top">
+                    SFT
+                </a>
             </div>
         </nav>
 
-
-
-        <div class="offcanvas offcanvas-start" tabindex="-1" id="offcanvasRight" aria-labelledby="offcanvasRightLabel">
+        <div class="offcanvas offcanvas-start" data-bs-scroll="true" tabindex="-1" id="offcanvasWithBothOptions" aria-labelledby="offcanvasWithBothOptionsLabel">
             <div class="offcanvas-header">
-                <h5 class="offcanvas-title" id="offcanvasRightLabel"><em>SFT</em> තක්සලාව</a></h5>
+                <h5 class="offcanvas-title" id="offcanvasWithBothOptionsLabel">Menu :</h5>
                 <button type="button" class="btn-close" data-bs-dismiss="offcanvas" aria-label="Close"></button>
             </div>
             <div class="offcanvas-body">
                 <div class="d-grid gap-2">
-                    <button class="btn btn-primary" type="button">Home</button>
-                    <button class="btn btn-primary" type="button">Other</button>
+                    <a class="btn btn-primary btn-sm" aria-current="page" href="Admin.php">Home</a>
+                    <a class="btn btn-outline-danger btn-sm" aria-current="page" href="../req/logout.php">Logout</a>
                 </div>
             </div>
         </div>
 
-    </div>
 
-
-    <script src="js/isotope.min.js"></script>
-    <script src="js/sftthaksalawacustom.js"></script>
-
-    <script src="../bootstrap/js/bootstrap.bundle.min.js"></script>
-</body>
+        <script src="../bootstrap/js/bootstrap.bundle.min.js"></script>
+    </body>
 
     </html>
 
