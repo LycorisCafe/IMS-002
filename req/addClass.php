20843a0f82797f5fd51404514bce58890e4f8760
naveenb2004 <naveennbalasooriya2004@gmail.com>
2023-01-04 00:00:33 +0530

    modifications on addClasses.php

--- req/addClass.php
+++ req/addClass.php
@@ -19,87 +19,94 @@ if (isset($_SESSION['id']) && isset($_SESSION['role'])) {
         <script src="../fontawesome.com.js" crossorigin="anonymous"></script>
     </head>
 
-<?php
-      if(isset($_POST['submit']))  
-      {
-            $insName = $_POST['insName'];
-            $city = $_POST['city'];
-            $al_year = $_POST['al_year'];
-            $day = $_POST['day'];
-            $time = $_POST['time'];
+    <?php
+    if (isset($_POST['submit'])) {
+        $insName = $_POST['insName'];
+        $city = $_POST['city'];
+        $al_year = $_POST['al_year'];
+        $day = $_POST['day'];
+        $time = $_POST['time'];
 
-            // db connection
-            include_once '../connection.php';
-            $sql = "INSERT INTO classes (al_year,  day, time,institute, city) VALUES ('$al_year', '$day', '$time', '$insName', '$city')";
-            $result = mysqli_query($con, $sql);
-            if($result)
-            {
-                echo "<script>alert('New Class adding completed!');</script>";
-            }else{
-                $em = "Error adding new class";
-                header("Location: addClass.php?error=$em");
-                exit;
-            }
-      }
-?>
+        // db connection
+        include_once '../connection.php';
+        $sql = "INSERT INTO classes (al_year,  day, time,institute, city) VALUES ('$al_year', '$day', '$time', '$insName', '$city')";
+        $result = mysqli_query($con, $sql);
+        if ($result) {
+            echo "<script>alert('New Class adding completed!');</script>";
+        } else {
+            $em = "Error adding new class";
+            header("Location: addClass.php?error=$em");
+            exit;
+        }
+    }
+    ?>
 
 
-<body>
-    <!-- Imports -->
-    <?php require_once "navbar.php"; ?>
-    <div class="container col-lg-4 col-md-5 align-self-center">
-						<div class="card" style="transform: translate(0%, 20%);">
-								<div class="card-header text-center">
-										<h3 class='display-5' style='color: #fff;'>New Class</h3>
-								</div>
-								<div class="card-body">
-										<form action="addClass.php"  method="POST">
-                                        <?php if(isset($_GET['error'])) { ?>
-                                            <div class='alert alert-danger' role='alert'>
-                                                <?=$_GET['error']?>
-                                            </div>
-						                <?php } ?>
-                                                <div class="col-auto mb-3">
-														<label class="form-label">Institute Name: </label>
-														<input type="text" class="form-control" name="insName" aria-describedby="insName" autocomplete="off" required placeholder="Apple">
-												</div>
-                                                <div class="col-auto mb-3">
-														<label class="form-label">City: </label>
-														<input type="text" class="form-control" name="city" aria-describedby="city" autocomplete="off" required placeholder="Galle">
-												</div>
-                                                <div class="col-auto mb-3">
-														<label class="form-label">A/L Year: </label>
-														<input type="text" class="form-control" name="al_year" aria-describedby="al_year" autocomplete="off" required placeholder="2022">
-												</div>
-												<div class="col-auto mb-3">
-														<label class="form-label">Day: </label>
-														<select class="form-control" name='day'>
-															<option value='1'>Monday</option>
-															<option value='2'>Tuesday</option>
-															<option value='3'>Wednesdy</option>
-															<option value='4'>Thursday</option>
-															<option value='5'>Froday</option>
-															<option value='6'>Saturday</option>
-															<option value='7'>Sunday</option>
-														</select>
-												</div>
-                                                <div class="col-auto mb-3">
-														<label class="form-label">Time: </label>
-														<input type="text" class="form-control" name="time" aria-describedby="time" autocomplete="off" required placeholder="EXAMPLE - 01:30 PM">
-												</div>
-                                                
-												<div class="d-grid gap-2">
-														<button type="submit" class="btn btn-primary" name='submit'>Add</button>
-												</div>
-										</form>
-								</div>
-						</div>
-				</div><br/><br/><br/><br/><br/><br/>
+    <body>
+        <!-- Imports -->
+        <?php require_once "navbar.php"; ?>
+        <div class="container col-lg-4 col-md-5 align-self-center">
+            <div class="card" style="transform: translate(0%, 20%);">
+                <div class="card-header text-center">
+                    <h3 class='display-5' style='color: #fff;'>New Class</h3>
+                </div>
+                <div class="card-body">
+                    <form action="addClass.php" method="POST">
+                        <?php if (isset($_GET['error'])) { ?>
+                            <div class='alert alert-danger' role='alert'>
+                                <?= $_GET['error'] ?>
+                            </div>
+                        <?php } ?>
+                        <div class="col-auto mb-3">
+                            <label class="form-label">Institute Name: </label>
+                            <input type="text" class="form-control" name="insName" aria-describedby="insName" autocomplete="off" required placeholder="Apple">
+                        </div>
+                        <div class="col-auto mb-3">
+                            <label class="form-label">City: </label>
+                            <input type="text" class="form-control" name="city" aria-describedby="city" autocomplete="off" required placeholder="Galle">
+                        </div>
+                        <div class="col-auto mb-3">
+                            <label class="form-label">A/L Year: </label>
+                            <input type="text" class="form-control" name="al_year" aria-describedby="al_year" autocomplete="off" required placeholder="2022">
+                        </div>
+                        <div class="col-auto mb-3">
+                            <label class="form-label">Day: </label>
+                            <select class="form-control" name='day'>
+                                <option value='1'>Monday</option>
+                                <option value='2'>Tuesday</option>
+                                <option value='3'>Wednesdy</option>
+                                <option value='4'>Thursday</option>
+                                <option value='5'>Froday</option>
+                                <option value='6'>Saturday</option>
+                                <option value='7'>Sunday</option>
+                            </select>
+                        </div>
+                        <div class="col-auto mb-3">
+                            <label class="form-label">Time: </label>
+                            <div class="input-group mb-3">
+                                <input type="text" placeholder="HH" class="form-control" aria-label="Text input with dropdown button">
+                                <input type="text" placeholder="MM" class="form-control" aria-label="Text input with dropdown button">
+                                <button class="btn btn-outline-secondary dropdown-toggle" type="button" data-bs-toggle="dropdown" aria-expanded="false">AM/PM</button>
+                                <ul class="dropdown-menu dropdown-menu-end">
+                                    <li><a class="dropdown-item" href="#">AM</a></li>
+                                    <li><a class="dropdown-item" href="#">PM</a></li>
+                                </ul>
+                            </div>
+                        </div>
 
-        
-<?php include 'footer.php'; ?>
-</body>
-</html>
+                        <div class="d-grid gap-2">
+                            <button type="submit" class="btn btn-primary" name='submit'>Add</button>
+                        </div>
+                    </form>
+                </div>
+            </div>
+        </div><br /><br /><br /><br /><br /><br />
+
+
+        <?php include 'footer.php'; ?>
+    </body>
+
+    </html>
 
 <?php } else {
     header("Location: ../login.php");
