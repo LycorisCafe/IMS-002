b59b840c1cf5c013cc2314109e59ac2492e36f6f
DasunNethsara-04 <techsaralk.pro@gmail.com>
2023-01-05 01:26:52 +0530

    exam.php done

--- req/exams.php
+++ req/exams.php
@@ -20,6 +20,60 @@ if (isset($_SESSION['id']) && isset($_SESSION['role'])) {
         <script src="../fontawesome.com.js" crossorigin="anonymous"></script>
     </head>
 
+<?php
+    if(isset($_POST['submit'])) {
+        $id = $_POST['id'];
+        $marks = $_POST['marks'];
+        $rank = $_POST['rank'];
+
+        // finding the grade
+        if($marks >= 0 and $marks <= 100) {
+            if($marks >= 75) {
+                $grade = "A";
+            } else if($marks >= 65) {
+                $grade = "B";
+            } else if($marks >= 55) {
+                $grade = "C";
+            } else if($marks >= 35) {
+                $grade = "S";
+            } else {
+                $grade = "W";
+            }
+        } else {
+            $em = "Marks should be 0 - 100 range!";
+            header("Location: exams.php?error=$em");
+            exit;
+        }
+
+        $date = date("Y-m-d");
+
+        // db
+        include_once '../connection.php';
+        $sql1 = "SELECT id FROM regclass WHERE studentId='$id'";
+        $result1 = mysqli_query($con, $sql1);
+        if(mysqli_num_rows($result1) > 0) {
+            $row1 = mysqli_fetch_assoc($result1);
+            $regclzid = $row1['id'];
+
+            $sql2 = "SELECT * FROM exam WHERE regclassID='$regclzid' AND date='$date'";
+            $result2 = mysqli_query($con, $sql2);
+            if(mysqli_num_rows($result2) == 0) {
+                $sql3 = "INSERT INTO exam (regclassID, date, marks, grade, rank) VALUES ('$regclzid', '$date', '$marks', '$grade', '$rank')";
+                $result3 = mysqli_query($con, $sql3);
+                echo "<script>alert('Marksheet updated!');</script>";
+            } else {
+                $em = "Marks already added!";
+                header("Location: exams.php?error=$em");
+                exit;
+            }
+        } else {
+            $em = "Invalid Student ID or Not found the iD";
+            header("Location: exams.php?error=$em");
+            exit;
+        }
+    }
+?>
+
 <body>
     <!-- Imports -->
     <?php require_once "navbar.php"; ?>
@@ -29,7 +83,7 @@ if (isset($_SESSION['id']) && isset($_SESSION['role'])) {
                                         <h3 class='display-5' style='color: #fff;'>Exam Result</h3>
                                 </div>
                                 <div class="card-body">
-                                        <form action="accounts.php"  method="POST">
+                                        <form action="exams.php"  method="POST">
                                         <?php if(isset($_GET['error'])) { ?>
                                             <div class='alert alert-danger' role='alert'>
                                                 <?=$_GET['error']?>
