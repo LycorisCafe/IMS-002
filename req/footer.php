f6865bd92c4c58735ce001f4dad42e76ce7b2f16
DasunNethsara-04 <techsaralk.pro@gmail.com>
2023-01-13 02:20:52 +0530

    footer restored

--- req/footer.php
+++ req/footer.php
@@ -1,4 +1,4 @@
 <link href="../bootstrap/css/bootstrap.min.css" rel="stylesheet">
 <div class="text-center mt-auto py-3 bg-dark"><br>
-        <!-- <h5 style="color: #fff;">&copy; COPYRIGHT SFT තක්සලාව | DESIGN BY LYCORIS CAFE</h5><br> -->
+        <h5 style="color: #fff;">&copy; COPYRIGHT SFT තක්සලාව | DESIGN BY LYCORIS CAFE</h5><br> 
 </div>
