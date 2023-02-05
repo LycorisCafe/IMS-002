e9c89448bbd61f758ceda6feab8756f825d40a43
randillasith <randillasith.me@.com>
2023-02-05 19:30:18 +0530

    UI Updates

---
+++ DB/mydb.sql
@@ -0,0 +1,256 @@
+-- phpMyAdmin SQL Dump
+-- version 5.2.0
+-- https://www.phpmyadmin.net/
+--
+-- Host: localhost
+-- Generation Time: Jan 26, 2023 at 05:12 AM
+-- Server version: 8.0.31
+-- PHP Version: 8.2.0
+
+SET SQL_MODE = "NO_AUTO_VALUE_ON_ZERO";
+START TRANSACTION;
+SET time_zone = "+00:00";
+
+
+/*!40101 SET @OLD_CHARACTER_SET_CLIENT=@@CHARACTER_SET_CLIENT */;
+/*!40101 SET @OLD_CHARACTER_SET_RESULTS=@@CHARACTER_SET_RESULTS */;
+/*!40101 SET @OLD_COLLATION_CONNECTION=@@COLLATION_CONNECTION */;
+/*!40101 SET NAMES utf8mb4 */;
+
+--
+-- Database: `mydb`
+--
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `attendance`
+--
+
+CREATE TABLE `attendance` (
+  `id` int NOT NULL,
+  `regclassId` int NOT NULL,
+  `date_` date NOT NULL,
+  `d2d` tinyint(1) NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4;
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `classes`
+--
+
+CREATE TABLE `classes` (
+  `id` int NOT NULL,
+  `al_year` int NOT NULL,
+  `day` int NOT NULL,
+  `time` varchar(8) NOT NULL,
+  `institute` varchar(50) NOT NULL,
+  `city` varchar(50) NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4;
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `exam`
+--
+
+CREATE TABLE `exam` (
+  `id` int NOT NULL,
+  `regclassID` int NOT NULL,
+  `date` date NOT NULL,
+  `marks` int NOT NULL,
+  `grade` varchar(2) NOT NULL,
+  `rank` int NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4;
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `login`
+--
+
+CREATE TABLE `login` (
+  `id` int NOT NULL,
+  `username` varchar(20) NOT NULL,
+  `password` varchar(20) NOT NULL,
+  `name` varchar(100) NOT NULL,
+  `lastLogin` varchar(22) CHARACTER SET utf8mb4 NOT NULL,
+  `type` varchar(11) NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4;
+
+--
+-- Dumping data for table `login`
+--
+
+INSERT INTO `login` (`id`, `username`, `password`, `name`, `lastLogin`, `type`) VALUES
+(3, 'maintainer', 'maintainer', 'Lycoris Cafe', '2023-01-26 05:11:17 AM', 'Maintainer');
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `payments`
+--
+
+CREATE TABLE `payments` (
+  `id` int NOT NULL,
+  `regclassID` int NOT NULL,
+  `year` int NOT NULL,
+  `month` int NOT NULL,
+  `status` tinyint(1) NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4;
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `regclass`
+--
+
+CREATE TABLE `regclass` (
+  `id` int NOT NULL,
+  `studentId` varchar(7) NOT NULL,
+  `classId` int NOT NULL,
+  `attendance` tinyint NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4;
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `students`
+--
+
+CREATE TABLE `students` (
+  `id` varchar(7) NOT NULL,
+  `admissionNo` varchar(7) NOT NULL,
+  `fname` varchar(30) NOT NULL,
+  `lname` varchar(30) NOT NULL,
+  `al_year` year NOT NULL,
+  `DOB` date NOT NULL,
+  `pic` varchar(255) NOT NULL,
+  `institute` varchar(10) NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4;
+
+--
+-- Indexes for dumped tables
+--
+
+--
+-- Indexes for table `attendance`
+--
+ALTER TABLE `attendance`
+  ADD PRIMARY KEY (`id`),
+  ADD KEY `regclassId` (`regclassId`,`date_`);
+
+--
+-- Indexes for table `classes`
+--
+ALTER TABLE `classes`
+  ADD PRIMARY KEY (`id`);
+
+--
+-- Indexes for table `exam`
+--
+ALTER TABLE `exam`
+  ADD PRIMARY KEY (`id`),
+  ADD KEY `regclassID` (`regclassID`);
+
+--
+-- Indexes for table `login`
+--
+ALTER TABLE `login`
+  ADD PRIMARY KEY (`id`);
+
+--
+-- Indexes for table `payments`
+--
+ALTER TABLE `payments`
+  ADD PRIMARY KEY (`id`),
+  ADD KEY `regclassID` (`regclassID`);
+
+--
+-- Indexes for table `regclass`
+--
+ALTER TABLE `regclass`
+  ADD PRIMARY KEY (`id`),
+  ADD KEY `studentId` (`studentId`,`classId`),
+  ADD KEY `regclassId` (`classId`);
+
+--
+-- Indexes for table `students`
+--
+ALTER TABLE `students`
+  ADD PRIMARY KEY (`id`);
+
+--
+-- AUTO_INCREMENT for dumped tables
+--
+
+--
+-- AUTO_INCREMENT for table `attendance`
+--
+ALTER TABLE `attendance`
+  MODIFY `id` int NOT NULL AUTO_INCREMENT;
+
+--
+-- AUTO_INCREMENT for table `classes`
+--
+ALTER TABLE `classes`
+  MODIFY `id` int NOT NULL AUTO_INCREMENT;
+
+--
+-- AUTO_INCREMENT for table `exam`
+--
+ALTER TABLE `exam`
+  MODIFY `id` int NOT NULL AUTO_INCREMENT;
+
+--
+-- AUTO_INCREMENT for table `login`
+--
+ALTER TABLE `login`
+  MODIFY `id` int NOT NULL AUTO_INCREMENT;
+
+--
+-- AUTO_INCREMENT for table `payments`
+--
+ALTER TABLE `payments`
+  MODIFY `id` int NOT NULL AUTO_INCREMENT;
+
+--
+-- AUTO_INCREMENT for table `regclass`
+--
+ALTER TABLE `regclass`
+  MODIFY `id` int NOT NULL AUTO_INCREMENT;
+
+--
+-- Constraints for dumped tables
+--
+
+--
+-- Constraints for table `attendance`
+--
+ALTER TABLE `attendance`
+  ADD CONSTRAINT `attendance_ibfk_1` FOREIGN KEY (`regclassId`) REFERENCES `regclass` (`id`) ON DELETE CASCADE ON UPDATE CASCADE;
+
+--
+-- Constraints for table `exam`
+--
+ALTER TABLE `exam`
+  ADD CONSTRAINT `exam_ibfk_1` FOREIGN KEY (`regclassID`) REFERENCES `regclass` (`id`) ON DELETE CASCADE ON UPDATE CASCADE;
+
+--
+-- Constraints for table `payments`
+--
+ALTER TABLE `payments`
+  ADD CONSTRAINT `payments_ibfk_1` FOREIGN KEY (`regclassID`) REFERENCES `regclass` (`id`) ON DELETE CASCADE ON UPDATE CASCADE;
+
+--
+-- Constraints for table `regclass`
+--
+ALTER TABLE `regclass`
+  ADD CONSTRAINT `regclass_ibfk_1` FOREIGN KEY (`classId`) REFERENCES `classes` (`id`) ON DELETE CASCADE ON UPDATE CASCADE,
+  ADD CONSTRAINT `regclass_ibfk_2` FOREIGN KEY (`studentId`) REFERENCES `students` (`id`) ON DELETE CASCADE ON UPDATE CASCADE;
+COMMIT;
+
+/*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
+/*!40101 SET CHARACTER_SET_RESULTS=@OLD_CHARACTER_SET_RESULTS */;
+/*!40101 SET COLLATION_CONNECTION=@OLD_COLLATION_CONNECTION */;
--- view/Moderator.php
+++ view/Moderator.php
@@ -1,199 +1,192 @@
 <?php
 session_start();
 if (isset($_SESSION['id']) && isset($_SESSION['role'])) {
-	?>
+?>
 
-
-	<!DOCTYPE html>
-	<html lang="en" data-bs-theme="dark">
+	<!doctype html>
+	<html lang="en">
 
 	<head>
 		<meta charset="utf-8">
 		<meta name="viewport" content="width=device-width, initial-scale=1">
-		<title>Moderator Panel</title>
-		<link rel="icon" type="image/x-icon" href="../Media/favicon.png">
-		<link href="../bootstrap/css/bootstrap.min.css" rel="stylesheet">
-		<link rel="stylesheet" href="../css/fonts.css">
-		<link rel="stylesheet" href="../css/temp.css">
-		<link href="../req/calendar.css" rel="stylesheet" type="text/css"> <!-- CSS for the calendar -->
-		<link href="../req/cal-area.css" rel="stylesheet" type="text/css"> <!-- CSS for the calendar body -->
-		<script src="../fontawesome.com.js" crossorigin="anonymous"></script>
+		<title>Modarator</title>
+		<script src="https://ajax.googleapis.com/ajax/libs/jquery/3.6.3/jquery.min.js"></script>
+		<link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.0-alpha1/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-GLhlTQ8iRABdZLl6O3oVMWSktQOp6b7In1Zl3/Jr59b6EGGoI1aFkw7cmDA6j6gD" crossorigin="anonymous">
 	</head>
 
-	<?php
-	$thisMonth = date("m");
-	$thisYear = date("Y");
-	$today = date("Y-m-s");
-	include_once '../connection.php';
-	$msg = "";
-	$regclzID = "";
-	if (isset($_POST['search'])) {
-		if (!empty($_POST['id'])) {
-			$stdID = $_POST['id'];
-			$sql1 = "SELECT * FROM students WHERE id='$stdID'";
-			$result1 = mysqli_query($con, $sql1);
-			if (mysqli_num_rows($result1) > 0) {
-				$row1 = mysqli_fetch_assoc($result1);
-				$id = $row1['id'];
-				$adNo = $row1['admissionNo'];
-				$name = $row1['fname'] . " " . $row1['lname'];
-				$img = $row1['pic'];
-				$sql2 = "SELECT id FROM regclass WHERE studentId='$id'";
-				$result2 = mysqli_query($con, $sql2);
-				$row2 = mysqli_fetch_assoc($result2);
-				$regclzID = $row2['id'];
-				$sql3 = "SELECT * FROM payments WHERE regclassID='$regclzID' AND year='$thisYear' AND month='$thisMonth'";
-				$result3 = mysqli_query($con, $sql3);
-				if (mysqli_num_rows($result3) > 0) {
-					$row3 = mysqli_fetch_assoc($result3);
-					if ($row3['status'] == "0") {
-						$msg = "Please pay the class fees!";
-					}
+	<body>
+
+
+
+		<nav class="navbar bg-body-tertiary" data-bs-theme="dark">
+			<div class="container-fluid">
+				<a class="navbar-brand">LycorisCafe</a>
+				<div class="d-flex d-grid gap-2">
+					<span style='color: #fff;' class="me-2">Role: <?php echo $_SESSION['role']; ?></span>
+					<span style='color: #fff;' class="me-2">User: <?php echo $_SESSION['name']; ?></span>
+					<a href="../req/logout.php" class="btn btn-danger">Logout</a>
+				</div>
+			</div>
+		</nav>
+		<br><br>
+		<div class="container col-sm-12 col-lg-6">
+			<div class="card text-center">
+				<div class="card-header">
+					<h2>Attendance Mark</h2>
+				</div>
+					<div class="d-grid gap-2">
+					<div class="card-body"><?php if (isset($_GET['error'])) { ?>
+						<div class='alert alert-danger' role='alert'>
+							<?= $_GET['error'] ?>
+						</div>
+					<?php } ?>
+						<form action="Moderator.php" method="post">
+							<input type="text" class="form-control" placeholder="Student ID" name="id">
+							<button class="btn btn-primary col-12" name="attend">Search and Mark Attend</button>
+							<button class="btn btn-outline-primary col-12" name="search">Search</button>
+						</form>
+					</div>
+				</div>
+			</div>
+		</div>
+
+		<?php
+		$img = "";
+		require_once '../connection.php';
+		if (isset($_POST['search'])) {
+			if (!empty($_POST['id'])) {
+				$id = $_POST['id'];
+				$sql1 = "SELECT * FROM students WHERE id='$id'";
+				$result1 = mysqli_query($con, $sql1);
+				if(mysqli_num_rows($result1) > 0) {
+					$row1 = mysqli_fetch_assoc($result1);
+					$name = $row1['fname']." ". $row1['lname'];
+					$id = $row1['id'];
+					$admissionNo = $row1['admissionNo'];
+					$img = $row1['pic'];
 				} else {
-					$msg = "Please pay the class fees!";
+					$em = "Invalid Student ID";
+					header("Location: Moderator.php?error=$em");
+					exit;
 				}
 			} else {
-				$em = "Invalid Student ID";
+				$em = "Please enter the Student ID";
 				header("Location: Moderator.php?error=$em");
 				exit;
 			}
-		} else {
-			$em = "Student ID Required!";
-			header("Location: Moderator.php?error=$em");
-			exit;
 		}
-	}
-
-	if(isset($_POST['submit'])) {
-		$msg = "";
-		$p = $_POST['paid'];
-		$sql4 = "SELECT * FROM attendance WHERE regclassId='$regclzID' AND date_='$today'";
-		$result4 = mysqli_query($con, $sql4);
-		if(mysqli_num_rows($result4) < 1) {
-			$sql5 = "UPDATE	regclass SET attendance='1' WHERE id='$regclzID'";
-			if(mysqli_query($con, $sql5)) {
-				$sql5 = "INSERT INTO attendance (regclassId, date_) VALUES ('$regclzID', '$today')";
-				$result5 = mysqli_query($con, $sql5);
-				if($p) {
-					$sql6 = "SELECT * FROM payments WHERE regclassId='$regclzID' AND year='$thisYear' AND month='$thisMonth'";
-					$result6 = mysqli_query($con, $sql6);
-					if(mysqli_num_rows($result6) < 1) {
-						$sql7 = "INSERT INTO payments (regclassId, year, month, status, pDate) VALUES ('$regclzID', '$thisYear', '$thisMonth', '1', '$today')";
-						if(mysqli_query($con, $sql7)) {
-							//
+		if(isset($_POST['attend'])) {
+			if (!empty($_POST['id'])) {
+				$id = $_POST['id'];
+				$today = date("Y-m-d");
+				$sql1 = "SELECT * FROM students WHERE id='$id'";
+				$result1 = mysqli_query($con, $sql1);
+				if(mysqli_num_rows($result1) > 0) {
+					$row1 = mysqli_fetch_assoc($result1);
+					$name = $row1['fname']." ". $row1['lname'];
+					$id = $row1['id'];
+					$admissionNo = $row1['admissionNo'];
+					$img = $row1['pic'];
+					$msg = "";
+					$sql2 = "SELECT * FROM regclass WHERE studentId='$id'";
+					$result2 = mysqli_query($con, $sql2);
+					$row2 = mysqli_fetch_assoc($result2);
+					$regclzid = $row2['id'];
+					$sql3 = "UPDATE regclass SET attendance='1' WHERE id='$regclzid'";
+					$result3 = mysqli_query($con, $sql3);
+					if($result3) {
+						$sql4 = "SELECT * FROM attendance WHERE regclassId='$regclzid' AND date_='$today'";
+						$result4 = mysqli_query($con, $sql4);
+						if(mysqli_num_rows($result4) < 1) {
+							$sql5 = "INSERT INTO attendance (regclassId, date_, d2d) VALUE ('$regclzid', '$today', '')";
+							if(mysqli_query($con, $sql5)) {
+								$msg = "Attendance Marked successfully!";
+								echo "<script>showToast();</script>";
+							}
+						} else {
+							$em = "Already marked the attendance!";
+							header("Location: Moderator.php?error='$em'");
+							exit;
 						}
-					} else {
-						$msg = "Already paid the class fees!";
 					}
+					
+				} else {
+					$em = "Invalid Student ID";
+					header("Location: Moderator.php?error=$em");
+					exit;
 				}
+			} else {
+				$em = "Enter the Student ID";
+				header("Location: Moderator.php?error=$em");
+				exit;
 			}
-		} else {
-			$msg = "Already marked the attendance";
 		}
-	}
+		?>
 
-	?>
-
-	<body>
-		<!-- Navbar -->
-		<header class="main-header clearfix" role="header">
-			<div class="logo">
-				<a href="../index.php">Lycoris Cafe</a>
-			</div>
-			<a href="../index.php" class="menu-link"><i class="fa fa-bars"></i></a>
-			<nav id="menu" class="main-nav" role="navigation">
-				<ul class="main-menu">
-					<li style='color: #fff;'>User: <?php echo $_SESSION['name']; ?></li>
-					<li style='color: #fff;'>Role: <?php echo $_SESSION['role']; ?></li>
-					<li style='color: #fff;'>Last Login: <?php echo $_SESSION['lastLogin']; ?></li>
-					<li><a href='../req/logout.php' class='btn btn-danger'> Logout </a></li>
-
-				</ul>
-			</nav>
-		</header>
-
-		<!-- Content -->
-		<div class="container col-lg-6"><br><br><br><br><br><br><br><br><br><br>
+		<div class="container col-sm-12 col-lg-6">
+			<hr>
 			<div class="card text-center">
 				<div class="card-header">
-					<h4 class='display-6' style='color: #fff;'>Attendane Marking</h4>
+					<h3>Student Search</h3>
 				</div>
 				<div class="card-body">
-					<form action='Moderator.php' method='POST'>
-					<?php if ((isset($_POST['search']) && $msg != "")) { ?>
-							<div class='alert alert-danger' role='alert' id="msg">
-								<?php echo $msg; ?>
-							</div>
-						<?php } elseif (isset($_GET['error'])){?>
-							<div class='alert alert-danger' role='alert' id="msg">
-							<?php echo $_GET['error']; ?>
+					<div class="row g-3">
+						<div class="col-12">
+							<?php if(isset($_POST['searchAandAttend']) && $img != "") {
+								echo "<img src='$img' class='rounded' width='200' height='200' alt='student img'>";
+							} else {
+								echo "<img src='../Media/dummy.jpg' class='rounded' width='200' height='200' alt='student img'>";
+							}
+							?>
 						</div>
-						<?php } ?>
-						<div class="input-group mb-3">
-							<input type="text" class="form-control" placeholder="Enter Student ID!" name='id'
-								autocomplete="off">
+						<div class="col-12">
+							<h5 id="name">Name : <?php if(isset($_POST['search'])) {echo $name;}?></h5>
+							<h5 id="id">ID : <?php if(isset($_POST['search'])) {echo $id;}?></h5>
+							<h5 id="admision">Admission : <?php if(isset($_POST['search'])) {echo $admissionNo;}?></h5>
 						</div>
-						<div class="d-grid gap-2">
-							<input class="btn btn-primary" type="submit" name='search' value='Search'>
-						</div>
-						<hr>
-						<div>
-
-						</div><br />
-					</form>
-
-					<form action='Moderator.php' method='POST'>
-						<div class="mb-3">
-							<div class="rounded border border-success" style='font-size: 22px;'>
-								<b title='Student ID'><?php if (isset($_POST['search'])) {
-									echo "Student ID: <font color='#10A0FF'>$id</font>";
-								} ?></b>
-							</div>
-						</div>
-						<div class="mb-3">
-							<div class="rounded border border-success" style='font-size: 22px;'>
-								<b title='Admission Number'><?php if (isset($_POST['search'])) {
-									echo "Admission No.: <font color='#10A0FF'>$adNo</font>";
-								} ?></b>
-							</div>
-						</div>
-						<div class="mb-3">
-							<div class="rounded border border-success" style='font-size: 22px;'>
-								<b title='Student Name'><?php if (isset($_POST['search'])) {
-									echo "Student Name: <font color='#10A0FF'>$name</font>";
-								} ?></b>
-							</div>
-						</div>
-						<hr>
-						<div class="form-check text-start">
-							<input class="form-check-input" type="checkbox" id="flexCheckChecked" name='day2day'>
-							<label class="form-check-label" for="flexCheckChecked">Day 2 Day Paper</label>
-
-						</div>
-						<div class="form-check text-start">
-							<input class="form-check-input" type="checkbox" id="fees" name='paid'>
-							<label class="form-check-label" for="fees">Paid</label>
-						</div><br />
-
-						<div class="d-grid gap-2">
-							<input class="btn btn-primary" type="submit" name='submit' value='Mark as Attend!'>
-						</div>
-						<div class="d-grid gap-2">
-							<br /><button class="btn btn-warning" name="finish">Finish</button>
-						</div>
-
-
+					</div>
 				</div>
 			</div>
-			</form>
-		</div>
-		</div>
+			<hr>
 		</div>
 
-		<script src="../bootstrap/js/bootstrap.bundle.min.js"></script>
-		<script src="../js/isotope.min.js"></script>
-		<script src="../js/sftthaksalawacustom.js"></script>
-		<?php include '../req/footer.php'; ?>
+		<div class="container">
+			<div class="d-grid gap-2 col-lg-7 col-sm-12 mx-auto">
+				<button class="btn btn-danger" type="button">Mark as Absent</button>
+			</div>
+		</div>
+		<br><br>
+
+
+		<button type="button" class="btn btn-primary" id="liveToastBtn">Show live toast</button>
+
+		<div class="toast-container position-fixed bottom-0 end-0 p-3">
+			<div class="toast" id="liveToast" role="alert" aria-live="assertive" aria-atomic="true">
+				<div class="toast-body">
+					<?php //if(isset($_POST['attend']) && $msg != "") { echo $msg;} ?>
+					Hello
+					<div class="mt-2 pt-2 border-top">
+						<button type="button" class="btn btn-primary btn-sm">Take action</button>
+						<button type="button" class="btn btn-secondary btn-sm" data-bs-dismiss="toast">Close</button>
+					</div>
+				</div>
+			</div>
+		</div>
+
+		<script>
+			// const toastTrigger = document.getElementById('liveToastBtn')
+			function showToast() {
+				const toastLiveExample = document.getElementById('liveToast')
+				//if (toastTrigger) {
+					toastTrigger.addEventListener('click', () => {
+						const toast = new bootstrap.Toast(toastLiveExample)
+						toast.show();
+					})
+				//}
+			}
+		</script>
+
+		<script src="https://cdn.jsdelivr.net/npm/bootstrap@5.3.0-alpha1/dist/js/bootstrap.bundle.min.js" integrity="sha384-w76AqPfDkMBDXo30jS1Sgez6pr3x5MlQ1ZAGC+nuZB+EYdgRZgiwxhTBTkF7CXvN" crossorigin="anonymous"></script>
 	</body>
 
 	</html>
