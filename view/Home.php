91ca50a2d0a9269388d951be261af99095098a5c
Naveen Balasooriya <95101859+naveenb2004@users.noreply.github.com>
2023-02-11 23:39:34 +0530

    Rollback

--- view/Home.php
+++ view/Home.php
@@ -64,7 +64,7 @@ if (isset($_SESSION['id']) && isset($_SESSION['role'])) {
 						<i class="fa fa-user fs-1"></i><br/>
 						Accounts
 					</a>
-					<a href="../req/logout.php" class="col btn btn-warning" >
+					<a href="../req/logout.php" class="col btn btn-warning m-2 py-5" >
 						<i class="fa fa-sign-out fs-1"></i><br/>
 						Logout
 					</a>
